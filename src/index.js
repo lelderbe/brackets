9d9b276670813da118e4eae3a9ea630870817ec1
lelderbe <fvdc@ya.ru>
2022-08-06 18:27:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const brackets = bracketsConfig.reduce(
+    (acc, item) => Object.assign(acc, { [item[0]]: item[1] }),
+    {},
+  );
+
+  for (const char of str) {
+    const last = stack.pop();
+
+    if (!last) {
+      stack.push(char);
+    } else if (char !== brackets[last]) {
+      stack.push(last);
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
+};
