89c7739432b7fed6803c03f3a3c16a0762dfd7ac
lelderbe <fvdc@ya.ru>
2023-08-08 09:32:21 +0300

    refactor: add simpler solution

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
+  const brackets = {};
+  for (const [open, close] of bracketsConfig) {
+    brackets[close] = open;
+  }
+
   const stack = [];
-  const brackets = bracketsConfig.reduce(
-    (acc, item) => Object.assign(acc, { [item[0]]: item[1] }),
-    {},
-  );
-
   for (const char of str) {
-    const last = stack.pop();
-
-    if (!last) {
-      stack.push(char);
-    } else if (char !== brackets[last]) {
-      stack.push(last);
+    if (char in brackets && brackets[char] === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
       stack.push(char);
     }
   }
